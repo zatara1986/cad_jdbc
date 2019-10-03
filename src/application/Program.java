aace29ae5704bdc7afde705849a4c826e6ed16f0
Wellington Thiago da Silva <zatara198@gmail.com>
2019-10-02 22:26:42 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,11 +13,13 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById =====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
-				
 		
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+		list.forEach(System.out::println);
 	}
 	
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.Conexao;
@@ -60,12 +61,7 @@ public class SellerDaoJDBC implements SellerDao{
 			rs = ps.executeQuery();
 			
 			if(rs.next()){
-				seller = new Seller(rs.getInt("id"), 
-									rs.getString("name"), 
-									rs.getString("email"), 
-									rs.getTimestamp("birthdate"), 
-									rs.getDouble("basesalary"), 
-									new Department(rs.getInt("departmentid"), rs.getString("DepName")));
+				seller = instatiateSeller(rs);
 			}
 		}catch(SQLException e){
 			throw new DbException(e.getMessage());
@@ -81,6 +77,51 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll(Integer id) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		List<Seller> list = new ArrayList<Seller>();
+		
+		StringBuilder sbSQL = new StringBuilder();
+		sbSQL.append(" SELECT ");
+		sbSQL.append("  s.id, s.name, s.email, s.birthdate,");
+		sbSQL.append("  s.basesalary,  s.departmentid, d.Name as DepName");
+		sbSQL.append(" FROM ");
+		sbSQL.append("  seller s ");
+		sbSQL.append("  INNER JOIN department d ON s.DepartmentId = d.Id ");
+		sbSQL.append(" WHERE s.departmentid = ? ");
+		sbSQL.append(" ORDER BY s.name ");
+		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try{
+			ps = conn.prepareStatement(sbSQL.toString());
+			ps.setInt(1, department.getId());
+			rs = ps.executeQuery();
+			
+			while(rs.next()){
+				list.add(instatiateSeller(rs));
+			}
+		}catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}finally{
+			Conexao.closePreparedStatement(ps);
+			Conexao.closeResultSet(rs);
+		}
+		
+		return list;
+	}
+	
+	private Seller instatiateSeller(ResultSet rs) throws SQLException{
+		return new Seller(rs.getInt("id"), 
+						  rs.getString("name"), 
+						  rs.getString("email"), 
+						  rs.getTimestamp("birthdate"), 
+						  rs.getDouble("basesalary"), 
+						  new Department(rs.getInt("departmentid"), rs.getString("DepName")));
 	}
 
 }
